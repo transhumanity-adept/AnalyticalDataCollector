204fe31d2abbe299d83b88c1eaeeeec782666057
Данила Бондаренко <bda@pkzdrav.ru>
2022-12-25 17:25:22 +0300

    add docker

--- start-app.bat
+++ start-app.bat
@@ -1,3 +1,3 @@
-start cmd.exe /C "color 3f && echo START BACKEND SERVICES && cd backend/WebApi && dotnet run"
-start cmd.exe /C "color 3f && echo START FRONTEND SERVICES && cd frontend && npm run dev"
-start http://localhost:3000/
+start cmd.exe /K "echo START BACKEND SERVICES && cd backend && docker build -t analytical-data-collector-backend . && docker run -it -p 5020:80 --rm --name analytical-data-collector-b analytical-data-collector-backend"
+start cmd.exe /K "echo START FRONTEND SERVICES && cd frontend && docker build -t analytical-data-collector-frontend . && docker run -it -p 8080:8080 --rm --name analytical-data-collector-f analytical-data-collector-frontend"
+start http://localhost:8080/
